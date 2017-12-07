3613762883c4b59d9c20df83af3307c20ca51bd0
Prachi Bhardwaj <prachi.bhardwaj@journalism.cuny.edu>
2017-12-07 07:50:50 -0500

    navigation and css

--- main.js
+++ main.js
@@ -16,6 +16,18 @@ var timer;
 
 $(document).ready(function () {
 
+	$(".goto").on("click", function () {
+		if ($(".dropdown").hasClass("show")) {
+			$(".dropdown").removeClass("show");
+		} else {
+			$(".dropdown").addClass("show");
+		}
+	});
+
+	$(".dropdown a").on("click", function () {
+		$(".dropdown").removeClass("show");
+	});
+
 	$(window).on("scroll", function () { //"scroll" is a reserved word that starts something on every pixel
 
 		//Obviously, any previously showing sidebars should be removed prior to showing the new sidebar
@@ -24,33 +36,40 @@ $(document).ready(function () {
 		} else {
 			$(".privacybar").removeClass("active");
 		}
+
+		if (!(isInView($("#header")))) {
+			$(".nav").addClass("active");
+		} else {
+			$(".nav").removeClass("active");
+		}
+
 	});
-});
 
-$("input[type='radio']").on("click", function () {
-	var price = 0;
-	var percent = 0;
+	$("input[type='radio']").on("click", function () {
+		var price = 0;
+		var percent = 0;
 
-	$("input[type='radio']:checked").each(function () {
-		var chosen = $(this)
-		var value = parseFloat(chosen.data("price"));
-		var rating = parseInt(chosen.data("rating"));
+		$("input[type='radio']:checked").each(function () {
+			var chosen = $(this)
+			var value = parseFloat(chosen.data("price"));
+			var rating = parseInt(chosen.data("rating"));
 
-		price = (price + value);
-		percent = percent + rating;
+			price = (price + value);
+			percent = percent + rating;
 
-		$(".price").text("$" + price.toFixed(2) + " per year");
-		$(".totalpercent").width(percent + "%");
+			$(".price").text("$" + price.toFixed(2) + " per year");
+			$(".totalpercent").width(percent + "%");
 
-		if (percent <= 30) {
-			$(".totalpercent").css("background-color", "#CD5C5C")
-		}
-		if (percent > 30 && percent < 60) {
-			$(".totalpercent").css("background-color", "#F4A460")
-		}
-		if (percent >= 60) {
-			$(".totalpercent").css("background-color", "#90EE90")
-		}
+			if (percent <= 30) {
+				$(".totalpercent").css("background-color", "#CD5C5C")
+			}
+			if (percent > 30 && percent < 60) {
+				$(".totalpercent").css("background-color", "#F4A460")
+			}
+			if (percent >= 60) {
+				$(".totalpercent").css("background-color", "#90EE90")
+			}
 
+		});
 	});
 });
